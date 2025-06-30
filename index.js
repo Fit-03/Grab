61e3af0e1257476ff45aead93eb5c7da2a879ba7
Fit-03 <b122410708@student.utem.edu.my>
2025-06-30 23:24:37 +0800

    Updated to test Azure

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(require('cors')());
 
